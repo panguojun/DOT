bcda02af9e37578b3dc36f00659e96cea190d301
panguojun <panguojun@users.noreply.github.com>
2022-11-06 11:40:52 +0000

    Update parser.hpp

--- parser.hpp
+++ parser.hpp
@@ -1,8 +1,37 @@
+class enum SYMBOL
+{
+    ARROW,
+    DOT,
+    CIRCLE,
+    ROUND
+}
+bool reached(int x, int y)
+{
+    // todo
+}
+symbol getsymbol(int x, int y)
+{
+    symbol s;
+    // todo
+    return s;
+}
 void parser(const image& img)
 {
     for(int i = 0; i < img.width; i ++)
+    for(int j = 0; j < img.height; j ++)
     {
-      pixel p = img(i, j);
-      
+        if(reached(i, j))
+            continue;
+        pixel p = img(i, j);
+        symbol s = getsymbol(i, j);
+        if(s.type == SYMBOL::DOT)
+        {
+        }
+        else if(s.type == SYMBOL::ARROW)
+        {
+        }
+        else if(s.type == SYMBOL::CIRCLE)
+        {
+        }
     }
 }
