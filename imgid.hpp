47c05afc4fe7b1e39b32a331d446c020d7f7da3a
panguojun <panguojun@users.noreply.github.com>
2022-11-08 18:01:38 +0800

    Update imgid.hpp

--- imgid.hpp
+++ imgid.hpp
@@ -10,14 +10,15 @@
 #define GETBLUE(rgb)    GetRValue(rgb)
 scope imageid
 {
-    static int SIZEX = 256;						// 区域尺度
+    static int SIZEX = 256;						 // 区域尺度
     static int SIZEY = 256;
+    static int cycle_depth = 888;                // 周期
+    static int wavlen = 28;                      // 波长
+
     static CDymPng png;
     static char* hismap = 0;
-    static int cycle_depth = 18;                 // 周期
-    static int wavlen = 10;                      // 周期
 
-    bool walk(rvec2 v, int ix, int iy, int depth, int phase, std::function<bool(int ix, int iy)> fun)
+    bool walk(int ix, int iy, int depth, int phase, std::function<bool(int ix, int iy)> fun)
     {
         if (ix < 0 || ix > SIZEX - 1)
         {
@@ -38,13 +39,14 @@ scope imageid
         }
         if (depth + 1 < cycle_depth)
         {
-            vec2 nv;
-            walk(nv, ix + 1, iy, depth + 1, phase + 1, fun);
-            walk(nv, ix, iy + 1, depth + 1, phase + 1, fun);
-            walk(nv, ix - 1, iy, depth + 1, phase + 1, fun);
-            walk(nv, ix, iy - 1, depth + 1, phase + 1, fun);
-
-            v = nv;
+            walk(ix + 1, iy, depth + 1, phase + 1, fun);
+            walk(ix, iy + 1, depth + 1, phase + 1, fun);
+            walk(ix - 1, iy, depth + 1, phase + 1, fun);
+            walk(ix, iy - 1, depth + 1, phase + 1, fun);
+            walk(ix - 1, iy - 1, depth + 1, phase + 1, fun);
+            walk(ix + 1, iy - 1, depth + 1, phase + 1, fun);
+            walk(ix - 1, iy + 1, depth + 1, phase + 1, fun);
+            walk(ix + 1, iy + 1, depth + 1, phase + 1, fun);
         }
         return false;
     }
@@ -52,70 +54,138 @@ scope imageid
     {
         memset(hismap, 0, SIZEX * SIZEY);
     }
-    int getPixelCor(int x, int y)
+    // ---------------------------------
+    // image
+    // ---------------------------------
+    int get_pixel(int x, int y)
     {
-        if (png.Width() > 0)
+        x < 0 ? x = 0 : 0;
+        y < 0 ? y = 0 : 0;
+        x > png.Width()  ? x = png.Width() : 0;
+        y > png.Height() ? y = png.Height() : 0;
         {
             y = png.Height() - 1 - y; // 上下颠倒
             return png.gifGetPixel(x, y);
         }
         return 0;
     }
-    int distance(int x1, int x2, int y1, int y2)
+    bool hittest(int x, int y)
     {
-        return (int)sqrt((x1 - x2)* (x1 - x2) + (y1 - y2) * (y1 - y2));
+        return GETRED(get_pixel(x, y)) > 50;
+    }
+    // ---------------------------------
+    // math
+    // ---------------------------------
+    int distance(int x1, int y1, int x2, int y2)
+    {
+        return (int)sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
     }
     real angle(int x1, int x2, int y1, int y2)
     {
         vec2 v1(x1, y1);
         vec2 v2(x2, y2);
-        real ang = dot(v1.normcopy(),v1.normcopy());
+        real ang = dot(v1.normcopy(),v2.normcopy());
         return ang;
     }
+    int igrad(int x, int y)
+    {
+        int con_num = 0;
+        con_num += hittest(x - 1, y);
+        con_num += hittest(x, y - 1);
+        con_num += hittest(x + 1, y);
+        con_num += hittest(x, y + 1);
+        con_num += hittest(x - 1, y - 1);
+        con_num += hittest(x + 1, y - 1);
+        con_num += hittest(x - 1, y + 1);
+        con_num += hittest(x + 1, y + 1);
+       
+        return con_num;
+    }
+    // ---------------------------------
+    // logic
+    // ---------------------------------
     bool isend(int x, int y)
     {
-        return true;
+        return igrad(x, y) < 2;
     }
     bool isarrow(int x, int y)
     {
-        return true;
+        return igrad(x, y) >= 4;
     }
     bool iscircle(int x, int y)
     {
         return true;
     }
+    // ---------------------------------
+    // walk
+    // 几何，代数，周期性, PI, E
+    // ---------------------------------
     void walk()
     {
         clearhistory();
         if (png.Width() == 0)
         {
-           ASSERT( png.LoadFromFileA("C:/Users/18858/Desktop/test.png"))
+           ASSERT(png.LoadFromFileA("C:/Users/18858/Desktop/test.png"))
         }
         for (int i = 1; i < SIZEX; i++) {
             for (int j = 1; j < SIZEY; j++)
             {
-                int cor = getPixelCor(i, j);
-                if (GETRED(cor) >= 50)
+                if (hismap[j * SIZEX + i] == 0 && hittest(i, j))
                 {
-                    vec2 v;
-                    walk(v, i, j, 0, 0, [i, j](int ix, int iy)->bool {
-                        int cor = getPixelCor(ix, iy);
-                        if (GETRED(cor) >= 50)
+                    vector<vec2> endpoints, arrows;
+                    PRINT("walk ------------------ ");
+                    int cor = RNDCOR;
+                    walk(i, j, 0, 0, [i, j, &endpoints, &arrows, cor](int ix, int iy) ->bool {
+                        if (hittest(ix, iy))
                         {
-                            if (distance(i, j, ix, iy) > wavlen)
-                                return false;
-                            else if(isend(ix, iy))
-                                return true;
-                            else if(isarrow(ix, iy))
-                                return true;
-                            else if(iscircle(ix, iy))
-                                return true;
-
-                            pixel(ix, iy, 0xFF00FFff);
+                            if (isend(ix, iy))
+                            {
+                               // PRINT("END");
+                                pointi(ix, iy, 2, 0xFF00FFff);
+                                endpoints.emplace_back(ix, iy);
+                               // if (distance(i, j, ix, iy) > wavlen)
+                                 //   return false;
+                            }
+                            if (isarrow(ix, iy))
+                            {
+                                //PRINT("ARROW " << ix << "," << iy);
+                                arrows.emplace_back(ix, iy);
+                                pointi(ix, iy, 2, 0xFFFF00ff);
+                               // if (distance(i, j, ix, iy) > wavlen)
+                                //    return false;
+                            }
+                           // PRINT(ix <<","<<iy);
+                            pixel(ix, iy, cor);
                             return true;
                         }
                         return false;
                     });
+                    {
+                        if (!endpoints.empty() && !arrows.empty())
+                        {
+                            for (int j = 0; j < arrows.size(); j++)
+                            {
+                                real mdis = 0;
+                                int index = 0;
+                                for (int i = 0; i < endpoints.size(); i++)
+                                {
+                                    crauto em = endpoints[i];
+                                    real dis = (em - arrows[j]).len();
+                                    if (dis > mdis)
+                                    {
+                                        index = i;
+                                        mdis = dis;
+                                    }
+                                }
+                                if (mdis > wavlen)
+                                {
+                                    drawline(endpoints[index] / IMAGESCALE, arrows[j] / IMAGESCALE, 0xFF00FF00);
+                                    PRINT("============ARROW " << endpoints[index].x << "," << endpoints[index].y << "->" << arrows[j].x << "," << arrows[j].y);
+                                    break;
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
